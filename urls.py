e7e1002face386e104295a7d212ca1e46cb446cd
Madhuban Khatri <madhuban211998@gmail.com>
2021-05-23 21:47:35 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,10 +1,25 @@
-from django.urls import path
-from . import views
+"""dj_gallary URL Configuration
 
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-	path('', views.IndexView.as_view(), name='index'),
-	path('upload/', views.UploadView.as_view(), name='upload'),
-	path('delete/<int:id>/', views.DeleteView.as_view(), name='delete_img')
-]
+    path('admin/', admin.site.urls),
+    path('', include('main.urls'))
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
